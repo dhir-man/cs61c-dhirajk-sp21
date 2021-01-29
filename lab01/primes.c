39f857bc2e0c2c4cab25d9146fa9e4d79f639c70
Dhiraj Koppanathi <dhirajko007@gmail.com>
2021-01-28 21:46:11 -0800

    changed primes and answers

--- lab01/primes.c
+++ lab01/primes.c
@@ -17,8 +17,7 @@ int main(int argc, char *argv[]) {
     /*Allows for an optional command line input. This is only for autograder testing of your code; you should not use the command line input for this exercise.*/
     int onepercent = (argc == 2 ? atoi(argv[1]) : MAXVAL);
     /*You may assume that the above line is correct*/
-    int i = 2;
-    for(;i<onepercent*100;i++) //So that percent messages are easier to set up.
+    for(int i = 1;i<onepercent*100;i++) //So that percent messages are easier to set up.
     {
         if(isPrime(i)) //Checks if a number is prime. As a reminder, 0 is false, and any nonzero value is true.
         {
