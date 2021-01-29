5b71f45e098e8034f6017dc729cf31b396225c4c
Dhiraj Koppanathi <dhirajko007@gmail.com>
2021-01-29 00:17:38 -0800

    tested to cyclic linked list

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if(head == NULL) {
+        return 0;
+    }
+    node *hare = head;
+    node *tortiose = head;
+    do {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+    } while(hare->next->next != NULL && hare != tortiose);
+    if(tortoise == hare) {
+        return 1;
+    }
     return 0;
 }
