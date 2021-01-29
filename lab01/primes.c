95eee21cea6f18e13c79c4193d6633f08b188fb7
Dhiraj Koppanathi <dhirajko007@gmail.com>
2021-01-28 23:31:38 -0800

    primes redone

--- lab01/primes.c
+++ lab01/primes.c
@@ -5,13 +5,14 @@
 //Returns 1 if the input is a prime number, and 0 otherwise.
 int isPrime(int n)
 {
-    if(n == 2) {
-        return 1;
+    if(n%2 == 0){
+        if (n == 2) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
-    else if(n > 2 && n%2 == 0) {
-        return 0;
-    }
-    for(int i = 1; i < n; i+=2)
+    for(int i = 3; i < n; i+=2)
     {
         if(n%i == 0) return 0;
     }
@@ -21,18 +22,18 @@ int isPrime(int n)
 int main(int argc, char *argv[]) {
     /*Allows for an optional command line input. This is only for autograder testing of your code; you should not use the command line input for this exercise.*/
     int onepercent = (argc == 2 ? atoi(argv[1]) : MAXVAL);
-    int num_prime = 0;
     /*You may assume that the above line is correct*/
-    for(int i = 1;i<onepercent*100;i++) //So that percent messages are easier to set up.
+    for(int i = 2;i<onepercent*100;i++) //So that percent messages are easier to set up.
     {
+        int num_primes = 0;
         if(isPrime(i)) //Checks if a number is prime. As a reminder, 0 is false, and any nonzero value is true.
         {
-            fprintf(stdout, "%d\n", i); //Prints the number i to standard output
-            num_prime++;
+            fprintf(stdout, "%d", i); //Prints the number i to standard output
+            num_primes++;
         }
-        if(i % onepercent == 0) {
-            fprintf(stderr, "%d%% complete", (i%onepercent) * onepercent);
+        if (i != 0 && i % onepercent == 0) {
+            fprintf(stderr, "%d%% complete", (i / (onepercent * 100)));
         }
     }
-    fprintf(stderr, "Total primes found: %d\n", num_prime);
+    fprintf(stderr, "Total primes found: %d", num_primes);
 }
