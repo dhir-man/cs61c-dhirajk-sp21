1c9a554f5eb582bda3b5548db578c118c693766a
Dhiraj Koppanathi <dhirajko007@gmail.com>
2021-01-28 22:03:15 -0800

    primes.c modified

--- lab01/primes.c
+++ lab01/primes.c
@@ -16,12 +16,16 @@ int isPrime(int n)
 int main(int argc, char *argv[]) {
     /*Allows for an optional command line input. This is only for autograder testing of your code; you should not use the command line input for this exercise.*/
     int onepercent = (argc == 2 ? atoi(argv[1]) : MAXVAL);
+    int num_prime = 0;
     /*You may assume that the above line is correct*/
     for(int i = 1;i<onepercent*100;i++) //So that percent messages are easier to set up.
     {
         if(isPrime(i)) //Checks if a number is prime. As a reminder, 0 is false, and any nonzero value is true.
         {
-            fprintf(stdout, "%d", i); //Prints the number i to standard output
+            fprintf(stdout, "%d", i"\n"); //Prints the number i to standard output
+            printf("%e%% complete\n", onepercent*100);
+            num_prime++;
         }
     }
+    printf("Total primes found: %d\n", num_prime);
 }
